2e41f606e53bf98889fc66cd76560fa270bdbfca
Jack Cheng <randomno@hotmail.com>
2021-05-18 00:18:57 +0800

    create environment

---
+++ js/all.js
@@ -0,0 +1,26 @@
+const url = 'https://vue3-course-api.hexschool.io';
+
+const logIn = document.querySelector(".logIn");
+
+
+logIn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const username = document.querySelector("#username").value;
+  const password = document.querySelector("#password").value;
+  const user = {
+    username,
+    password
+  }
+  axios.post(`${url}/admin/signin`,user)
+  .then(res=>{
+    // console.log("執行then",res);
+    if(res.data.success === true){
+      const token = res.data.token;
+      const expired = res.data.expired;
+      document.cookie = `weekTwoToken=${token}; expires=${expired}`;
+    }
+  })
+  .catch(err=>{
+    console.log("執行catch",err);
+  })
+});
